bc682b61b3e28ba467f4f5b42bcbc1bc59eb4d19
RationalPrabal <prabal19999@gmail.com>
2023-01-17 23:23:00 +0530

    project setup

---
+++ baby-bliss/src/pages/_app.js
@@ -0,0 +1,9 @@
+import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+export default function App({ Component, pageProps }) {
+  return <ChakraProvider>
+<Component {...pageProps} />
+
+  </ChakraProvider>
+  
+}
