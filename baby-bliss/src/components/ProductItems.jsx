0d043ff828f45da1708829ec4c3b58dc8f010213
RationalPrabal <prabal19999@gmail.com>
2023-01-19 23:58:15 +0530

    day 3 work of fw21_1273 done

--- baby-bliss/src/components/ProductItems.jsx
+++ baby-bliss/src/components/ProductItems.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import { Button, GridItem,Img,Text,Box } from '@chakra-ui/react';
+import { Button, GridItem,Text,Box } from '@chakra-ui/react';
+import axios from 'axios';
 const ProductItems = ({id,image,price,title,mrp,discount,lft}) => {
+  const AddToCart=async()=>{
+   await axios.post("http://localhost:8080/cart",{
+      id,img:image,price,mrp,discount,lft,title
+  }
+  )
+  }
   return (
     <Box>
         <GridItem border={"2px solid red"} textAlign={"left"}>
@@ -13,14 +20,14 @@ lineHeight={"12px"}
 fontWeight={"400"}
 >{title}</Text>
 
-<Box display={"flex"} justifyContent={"space-between"} w="65%" mt="2">
+<Box display={"flex"} justifyContent={"space-between"} w="75%" mt="2">
 <Text as="b"> ₹ {price}</Text>
 <Text as="s"> ₹ {mrp}</Text>
 <Text color={"red"}>{discount}</Text>
 </Box>
 <Text fontSize={12}>{lft}</Text>
 <Box display={"flex"} mt="2">
-    <Button _hover={""} bg={"red"} color="white">ADD TO CART</Button>
+    <Button _hover={""} bg={"red"} color="white" onClick={AddToCart}>ADD TO CART</Button>
     <Button mx="3" border={"1px solid tomato"}>WISHLIST</Button>
 </Box>
 
--- baby-bliss/src/pages/footwears.jsx
+++ baby-bliss/src/pages/footwears.jsx
@@ -1,50 +1,50 @@
 import axios from 'axios'
 import React from 'react'
-import BoySidebar from '@/components/BoySidebar'
+import FootwearSidebar from '@/components/FootwearSidebar'
 import { Grid,Box } from '@chakra-ui/react'
 import ProductItems from '@/components/ProductItems'
-const boys = ({T_shirts}) => {
-  const [shirts,set_shirts]= React.useState(T_shirts)
+const boys = ({initialData}) => {
+  const [data,set_data]= React.useState(initialData)
   
 // function for sort by price
  const OrderPrice=async(order)=>{
-let res =  await axios.get(`http://localhost:8080/boys?_sort=price&_order=${order}`)
+let res =  await axios.get(`http://localhost:8080/shoes?_sort=price&_order=${order}`)
 console.log(res.data)
-set_shirts(res.data)
+set_data(res.data)
  }
 
  // function for sort by discount
  const OrderDiscount=async(order)=>{
-  let res =  await axios.get(`http://localhost:8080/boys?_sort=discount&_order=${order}`)
+  let res =  await axios.get(`http://localhost:8080/shoes?_sort=discount&_order=${order}`)
   console.log(res.data)
-  set_shirts(res.data)
+  set_data(res.data)
    }
 
 
 
   // function for filtering by price
   const FilterPrice=async(lowerRange,higherRange)=>{
-    let res =  await axios.get(`http://localhost:8080/boys?price_gte=${lowerRange}&price_lte=${higherRange}`)
+    let res =  await axios.get(`http://localhost:8080/shoes?price_gte=${lowerRange}&price_lte=${higherRange}`)
     console.log(res.data)
-    set_shirts(res.data)
+    set_data(res.data)
      }
 
      // function for filtering by category
 
      const FilterCategory=async(query)=>{
-      let res =  await axios.get(`http://localhost:8080/boys?q=${query}`)
+      let res =  await axios.get(`http://localhost:8080/shoes?q=${query}`)
       console.log(res.data)
-      set_shirts(res.data)
+      set_data(res.data)
        }
  
   return (
     <Box display="flex" w={{base:"100%",sm:"100%", md:"80%"}} m="auto">
     <Box w={{base:"30%", sm:"30%",md:"20%"}}>
-      <BoySidebar OrderPrice={OrderPrice} OrderDiscount={OrderDiscount} FilterPrice={FilterPrice} FilterCategory={FilterCategory}/>
+      <FootwearSidebar OrderPrice={OrderPrice} OrderDiscount={OrderDiscount} FilterPrice={FilterPrice} FilterCategory={FilterCategory}/>
     </Box>
     <Box w={{base:"70%", sm:"70%",md:"80%"}}>
     <Grid templateColumns={{base:'repeat(1, 1fr)',sm:'repeat(2, 1fr)',md:'repeat(3, 1fr)'}} gap={{base:0,sm:2,md:6}} w={{base:"100%",sm:"100%",md:"100%"}}>
-    {shirts.map((el,id)=> <ProductItems key={id} title={el.title} price={el.price} image={el.img} mrp={el.mrp} id={id} discount={el.discount} lft={el.lft} />)
+    {data.map((el,id)=> <ProductItems key={id} title={el.title} price={el.price} image={el.img} mrp={el.mrp} id={el.id} discount={el.discount} lft={el.lft} />)
 }
         </Grid>
         </Box>
@@ -55,10 +55,10 @@ set_shirts(res.data)
 export default boys
 
 export async function getStaticProps(){
-    let response = await axios.get("http://localhost:8080/boys")
+    let response = await axios.get("http://localhost:8080/shoes")
     return {
         props:{
-        T_shirts: response.data
+        initialData: response.data
         }
       }
 }
