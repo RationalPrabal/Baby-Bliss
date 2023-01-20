ce11ac64636d0c476de2ac2abe2a733aba7c131d
salimzeeshan <zeeshasalim541@gmail.com>
2023-01-20 22:52:22 +0530

    day-4

--- baby-bliss/Component/Navbaar/NavbaarF.jsx
+++ baby-bliss/Component/Navbaar/NavbaarF.jsx
@@ -330,8 +330,8 @@
 
 
 import { Fragment } from 'react'
-// import { Disclosure, Menu, Transition } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
