5052de6dc587db7ca0cae73042777619d0f43744
Prabal <prabal19999@gmail.com>
2023-01-20 23:33:52 +0530

    added add to cart functionality

--- baby-bliss/src/pages/_app.js
+++ baby-bliss/src/pages/_app.js
@@ -5,9 +5,10 @@ import NavbaarF from 'Component/Navbaar/NavbaarF'
 import Footer from './footer'
 export default function App({ Component, pageProps }) {
   return <ChakraProvider>
-    <NavbaarF/>
+    {/* <NavbaarF/>
     <Navbaar/>
-
+ */}
+  <Component {...pageProps} />
 
     </ChakraProvider>
   
