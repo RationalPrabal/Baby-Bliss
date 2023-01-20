97fa315391614ca6dddbe11a74e4ddbb535e5f02
Prabal <prabal19999@gmail.com>
2023-01-21 01:00:06 +0530

    added health page and corrrected data of db

---
+++ baby-bliss/src/components/HealthSidebar.jsx
@@ -0,0 +1,225 @@
+import React from 'react'
+import { Box,Select,option,Text,Checkbox } from '@chakra-ui/react'
+const HealthSidebar = ({OrderPrice,OrderDiscount,FilterPrice,FilterCategory}) => {
+  return (
+    <Box >
+
+        {/* sort by price */}
+      <Text as="b">Sort By Price</Text>
+<Select onChange={(e)=>{
+    if(e.target.value=="option1"){
+        OrderPrice("asc")
+    }
+    else OrderPrice("desc")
+}} placeholder='Sort By Price'>
+  <option value='option1'>Low to High</option>
+  <option value='option2'>High to Low</option>
+
+</Select>
+
+{/* sort by discount */}
+<Text as="b">Sort By Discount</Text>
+<Select onChange={(e)=>{
+    if(e.target.value=="option1"){
+        OrderDiscount("asc")
+    }
+    else OrderDiscount("desc")
+}} placeholder='Sort By Discount'>
+  <option value='option1'>Low to High</option>
+  <option value='option2'>High to Low</option>
+
+</Select>
+
+{/* filter by price */}
+<Box display={"grid"}>
+  <Text as="b" >Filter by Price</Text>
+  <Checkbox onChange={(e)=>{if(e.target.checked){
+  
+      FilterPrice(0,300)
+    }
+  }
+  } colorScheme='red' >
+    Less than ₹ 300
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterPrice(300,500)
+}
+}
+} colorScheme='red' >
+    ₹ 300-₹ 500
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterPrice(500,1000)
+}
+}
+} colorScheme='red' >
+    ₹ 500-₹ 1000
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterPrice(1000,1500)
+}
+}
+} colorScheme='red' >
+    ₹ 1000-₹ 1500
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterPrice(1500,100000)
+}
+}
+} colorScheme='red' >
+    ₹ 1500 and More
+  </Checkbox>
+
+</Box>
+{/* filter by pack type */}
+<Box display={"grid"}>
+  <Text as="b" >Filter by Pack Type</Text>
+  
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("spray")
+}
+}
+} colorScheme='red' >
+    Spray
+  </Checkbox>
+  
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("dispenser")
+}
+}
+} colorScheme='red' >
+    Dispenser
+  </Checkbox>
+  
+
+</Box>
+
+
+{/* filter by color */}
+
+<Box display={"grid"}>
+  <Text as="b" >Filter by Color</Text>
+  
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("Navy")
+}
+}
+} colorScheme='red' >
+    Navy
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("black")
+}
+}
+} colorScheme='red' >
+    Black
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("brown")
+}
+}
+} colorScheme='red' >
+ Brown
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("grey")
+}
+}
+} colorScheme='red' >
+    Grey
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("White")
+}
+}
+} colorScheme='red' >
+    White
+  </Checkbox>
+  <Checkbox onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("red")
+}
+}
+} colorScheme='red' >
+Red
+</Checkbox>
+
+<Checkbox  onChange={(e)=>{if(e.target.checked){
+
+FilterCategory("orange")
+}
+}
+} colorScheme='red' >
+    Orange
+</Checkbox>
+<Checkbox  onChange={(e)=>{if(e.target.checked){
+
+FilterCategory("yellow")
+}
+}
+} colorScheme='red' >
+    Yellow
+</Checkbox>
+
+<Checkbox  onChange={(e)=>{if(e.target.checked){
+
+FilterCategory("multi")
+}
+}
+} colorScheme='red' >
+    Multi Color
+</Checkbox>
+<Checkbox  onChange={(e)=>{if(e.target.checked){
+
+FilterCategory("")
+}
+}
+} colorScheme='red' >
+    All
+</Checkbox>
+</Box>
+
+{/* filter by brand */}
+
+
+<Box display={"grid"}>
+  <Text as="b" >Filter by Brand</Text>
+  
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("babyhug")
+}
+}
+} colorScheme='red' >
+Babyhug
+  </Checkbox>
+  <Checkbox  onChange={(e)=>{if(e.target.checked){
+  
+  FilterCategory("chicco")
+}
+}
+} colorScheme='red' >
+Chicco
+  </Checkbox>
+  
+  
+</Box>
+
+
+    </Box>
+  
+  )
+}
+
+export default HealthSidebar
---
+++ baby-bliss/src/pages/health.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios'
+import React from 'react'
+import HealthSidebar from '@/components/HealthSidebar'
+import { Grid,Box } from '@chakra-ui/react'
+import ProductItems from '@/components/ProductItems'
+const boys = ({initialData}) => {
+  const [data,set_data]= React.useState(initialData)
+  
+// function for sort by price
+ const OrderPrice=async(order)=>{
+let res =  await axios.get(`http://localhost:8080/health?_sort=price&_order=${order}`)
+console.log(res.data)
+set_data(res.data)
+ }
+
+ // function for sort by discount
+ const OrderDiscount=async(order)=>{
+  let res =  await axios.get(`http://localhost:8080/health?_sort=discount&_order=${order}`)
+  console.log(res.data)
+  set_data(res.data)
+   }
+
+
+
+  // function for filtering by price
+  const FilterPrice=async(lowerRange,higherRange)=>{
+    let res =  await axios.get(`http://localhost:8080/health?price_gte=${lowerRange}&price_lte=${higherRange}`)
+    console.log(res.data)
+    set_data(res.data)
+     }
+
+     // function for filtering by category
+
+     const FilterCategory=async(query)=>{
+      let res =  await axios.get(`http://localhost:8080/health?q=${query}`)
+      console.log(res.data)
+      set_data(res.data)
+       }
+ 
+  return (
+    <Box display="flex" w={{base:"100%",sm:"100%", md:"80%"}} m="auto">
+    <Box w={{base:"30%", sm:"30%",md:"20%"}}>
+      <HealthSidebar OrderPrice={OrderPrice} OrderDiscount={OrderDiscount} FilterPrice={FilterPrice} FilterCategory={FilterCategory}/>
+    </Box>
+    <Box w={{base:"70%", sm:"70%",md:"80%"}}>
+    <Grid templateColumns={{base:'repeat(1, 1fr)',sm:'repeat(2, 1fr)',md:'repeat(3, 1fr)'}} gap={{base:0,sm:2,md:6}} w={{base:"100%",sm:"100%",md:"100%"}}>
+    {data.map((el,id)=> <ProductItems key={id} title={el.title} price={+el.price} image={el.img} mrp={el.mrp}  discount={el.discount}  />)
+}
+        </Grid>
+        </Box>
+        </Box>
+  )
+}
+
+export default boys
+
+export async function getStaticProps(){
+    let response = await axios.get("http://localhost:8080/health")
+    return {
+        props:{
+        initialData: response.data
+        }
+      }
+}
