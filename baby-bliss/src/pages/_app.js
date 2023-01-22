1abcef5e071837483423d21c479e256ea13e382c
murari_kaushal <murari.kaushal@bizbrolly.com>
2023-01-22 12:46:06 +0530

    toast added

--- baby-bliss/src/pages/_app.js
+++ baby-bliss/src/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
   return <ChakraProvider>
     {/* <NavbaarF/> */}
     <Navbaar/>
-
+     <Component {...pageProps}/>
 
     </ChakraProvider>
   
